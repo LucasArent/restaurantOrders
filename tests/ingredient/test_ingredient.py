1e9207191d509ebfe4372a10d8a9898fab374690
LucasArent <arentlucas2708@gmail.com>
2023-10-10 13:27:57 -0300

    feat 1REQ

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("queijo parmesão")
+    ingredient_2 = Ingredient("presunto")
+    ingredient_3 = Ingredient("presunto")
+
+    assert ingredient_1.__hash__() != ingredient_2.__hash__()
+    assert ingredient_1.__hash__() == ingredient_1.__hash__()
+    assert ingredient_1.__eq__(ingredient_2) is False  # == False
+    assert ingredient_2.__eq__(ingredient_3) is True  # == True
+    assert ingredient_1.__repr__() == "Ingredient('queijo parmesão')"
+    assert ingredient_1.name == "queijo parmesão"
+    assert ingredient_2.name == "presunto"
+    assert ingredient_1.restrictions == {Restriction.LACTOSE,
+                                  Restriction.ANIMAL_DERIVED}
